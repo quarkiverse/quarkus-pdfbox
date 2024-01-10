314dfe71f8426281a7211da2e016c77f15f66bb4
George Gastaldi <gegastaldi@gmail.com>
2024-01-10 16:54:44 -0300

    Add tests (#4)

--- integration-tests/src/main/java/io/quarkiverse/pdfbox/it/PdfBoxResource.java
+++ integration-tests/src/main/java/io/quarkiverse/pdfbox/it/PdfBoxResource.java
@@ -1,24 +1,32 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.quarkiverse.pdfbox.it;
+
+import java.io.InputStream;
+import java.net.URL;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 @Path("/pdfbox")
 @ApplicationScoped
@@ -26,7 +34,16 @@ public class PdfBoxResource {
     // add some rest methods here
 
     @GET
-    public String hello() {
-        return "Hello pdfbox";
+    @Path("/read-contents")
+    public String readPDFContents() throws Exception {
+        URL resource = getClass().getClassLoader().getResource("hello.pdf");
+        final byte[] content;
+        // Parse PDF using PDFBox
+        try (InputStream inputStream = resource.openStream()) {
+            content = IOUtils.toByteArray(inputStream);
+        }
+        PDDocument pdf = Loader.loadPDF(content);
+        PDFTextStripper stripper = new PDFTextStripper();
+        return stripper.getText(pdf).trim();
     }
 }
