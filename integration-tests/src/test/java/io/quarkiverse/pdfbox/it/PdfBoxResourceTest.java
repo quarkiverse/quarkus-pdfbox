af504a0b5bb658b8e8242e667113071d0278773b
George Gastaldi <gegastaldi@gmail.com>
2024-01-10 16:50:03 -0300

    Add tests

--- integration-tests/src/test/java/io/quarkiverse/pdfbox/it/PdfBoxResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/pdfbox/it/PdfBoxResourceTest.java
@@ -3,19 +3,22 @@ package io.quarkiverse.pdfbox.it;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PdfBoxResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void should_read_pdf_contents() {
         given()
-                .when().get("/pdfbox")
+                .when().get("/pdfbox/read-contents")
                 .then()
                 .statusCode(200)
-                .body(is("Hello pdfbox"));
+                .body(is("Hello PDFBox!"));
     }
 }
